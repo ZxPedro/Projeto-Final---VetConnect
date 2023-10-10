42c318c61ddb9e408da9865f7de151a9a7775680
ZxPedro <pedropaulo.pp807@gmail.com>
2023-10-09 22:43:02 -0300

    refactor: Adicionada opção de fechar o alerta

--- resources/views/users/user_create.blade.php
+++ resources/views/users/user_create.blade.php
@@ -10,8 +10,9 @@
 
         @if($errors->any())
         @foreach($errors->all() as $error)
-        <div class="alert alert-warning">
+        <div class="alert alert-warning alert-dismissible fade show" role="alert">
             {{ $error }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
         @endforeach
         @endif
